719c87f267eb7fee74fd6ebbacb37d33d69a1877
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-06-25 19:03:47 +0700

    feat: update user component to use dynamic image path (computed getter)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,4 +13,8 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get ImagePath(): string {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
 }
