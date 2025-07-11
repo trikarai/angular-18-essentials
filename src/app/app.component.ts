24ba34fee7bdf14ef435e695ae1b41f15047e249
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-07-11 17:54:58 +0700

    Add TaskComponent and integrate with user selection
    Introduces TaskComponent to display a user's name in the details section. Updates AppComponent to manage selected user state and passes the selected user's name to TaskComponent. Includes basic test and template for TaskComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
 
 import { DUMMY_USERS } from './user/dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS;
+  users = DUMMY_USERS.slice(1, 2);
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string) {
     console.log('Selected user:', id);
+    this.selectedUserId = id;
   }
 }
