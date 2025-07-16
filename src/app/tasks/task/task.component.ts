9dd6a4a21192ee0ee3fcd2be16f5bd6f2a3deaf8
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-07-16 13:56:10 +0700

    Display user-specific tasks with dynamic data
    Updated task and tasks components to accept and use userId and task data via @Input properties. TaskComponent now displays dynamic task details, and TasksComponent filters and passes relevant tasks to TaskComponent based on the selected user.

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+  id: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-
+  @Input({ required: true }) task!: Task;
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,7 +9,35 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({ required: true }) userId!: string | undefined;
+  @Input({ required: true }) name!: string | undefined;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Task 1',
+      dueDate: '2023-10-01',
+      summary: 'This is the first task.',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Task 2',
+      dueDate: '2023-10-01',
+      summary: 'This is the second task.',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Task 3',
+      dueDate: '2023-10-01',
+      summary: 'This is the third task.',
+    },
+  ]; // Example task array
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 
   onEditTask() {
     throw new Error('Method not implemented.');
