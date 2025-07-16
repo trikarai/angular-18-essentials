72b7c7a565ca3649312e86aee4c05d0983a83c0f
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-07-16 15:02:32 +0700

    Add form binding and task creation logic
    Implemented two-way binding for task form fields and added logic to emit new task data on creation. Also included FormsModule in component imports and reset form fields after task creation.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,9 +1,10 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -15,7 +16,23 @@ export class NewTaskComponent {
   }>();
   @Output() cancel = new EventEmitter<void>();
 
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
   onCancel() {
     this.cancel.emit();
+  }
+
+  onCreateTask() {
+    const taskData = {
+      title: this.enteredTitle,
+      dueDate: this.enteredDueDate,
+      summary: this.enteredSummary,
+    };
+    this.taskAdded.emit(taskData);
+    this.enteredTitle = '';
+    this.enteredSummary = '';
+    this.enteredDueDate = '';
   }
 }
