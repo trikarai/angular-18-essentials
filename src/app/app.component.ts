e5f98e598204efc8a75be0a072232a0ec5608b30
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-07-16 13:44:25 +0700

    Add TaskComponent and integrate with TasksComponent
    Introduced a new TaskComponent with its template, styles, and test file. Updated TasksComponent to use TaskComponent for displaying individual tasks. Refactored app.component to use TasksComponent instead of TaskComponent and fixed related imports and selectors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './tasks/tasks.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 import { DUMMY_USERS } from './user/dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
-  selector: 'app-task',
+  selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
-export class TaskComponent {
+export class TasksComponent {
   @Input() name: string | undefined;
 
   onEditTask() {
